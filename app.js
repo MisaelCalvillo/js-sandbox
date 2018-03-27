4f23f209c52d207319dadcdbc84e765d647711ef
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-03-26 22:05:26 -0600

    variables in javascript

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
-//Log to console
-console.log("Hello World");
-console.log(123);
-console.log(true);
-var greeting = 'Hello';
-console.log(greeting);
-console.log([1,2,3,4]);
-console.log({a:1, b:2});
-console.table({a:1, b:2});
+// // Variable declarations
+// // var, let, const
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
 
-console.error('This is some error');
-console.clear();
-console.warn('This is a warning');
-console.time('Hello');
-console.log('Hello World!');
-console.log('Hello World!');
-console.log('Hello World!');
-console.log('Hello World!');
-console.log('Hello World!');
-console.log('Hello World!');
-console.timeEnd('Hello');
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting = "Hello";
+// console.log(greeting);
 
-/*
-  multi
-  line
-  comments
-*/
+// // letters, numbers, _, $
+// // can not start with number
+// var _name = "John";
+
+// // Multi work vars
+// var firstName = 'John'; // Camel case
+// var first_name = 'Sara'; // Underscore
+// var FirstName = 'Tom'; // Pascal case
+// var firstname = "don't use!" // not recommended
+
+// // LET
+// let name;
+// name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// CONST
+// const name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+// const greeting;
+
+const person = {
+  name: 'John',
+  age: 30
+}
+
+person.name = 'Sara';
+person.age = 32;
+
+// console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+
+console.log(numbers);
+
+
 
