d8110519f22cc755d46f1403f788eb6bc5a754b8
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2018-03-26 22:23:17 -0600

    types of data in javascript PRIMITIVE and REFERENCE

--- app.js
+++ app.js
@@ -1,54 +1,29 @@
-// // Variable declarations
-// // var, let, const
-// var name = 'John Doe';
-// console.log(name);
-// name = 'Steve Smith';
-// console.log(name);
+// PRIMITIVE
 
-// // Init var
-// var greeting;
-// console.log(greeting);
-// greeting = "Hello";
-// console.log(greeting);
+// String
+const name = 'John Doe';
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Null
+const car = null;
+// Undefined
+let test;
+//Symbol
+const sym = Symbol();
 
-// // letters, numbers, _, $
-// // can not start with number
-// var _name = "John";
-
-// // Multi work vars
-// var firstName = 'John'; // Camel case
-// var first_name = 'Sara'; // Underscore
-// var FirstName = 'Tom'; // Pascal case
-// var firstname = "don't use!" // not recommended
-
-// // LET
-// let name;
-// name = 'John Doe';
-// console.log(name);
-// name = 'Steve Smith';
-// console.log(name);
-
-// CONST
-// const name = 'John Doe';
-// console.log(name);
-// name = 'Steve Smith';
-// console.log(name);
-// const greeting;
-
-const person = {
-  name: 'John',
-  age: 30
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ['movies', 'music'];
+// Object literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
 }
-
-person.name = 'Sara';
-person.age = 32;
-
-// console.log(person);
-
-const numbers = [1,2,3,4,5];
-numbers.push(6);
-
-console.log(numbers);
+// Dates
+const today = new Date();
+console.log(today);
 
 
-
+console.log(typeof today);
